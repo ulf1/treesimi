97e36bb4e68756a3992b1b40d12c784c722227d6
UH <554c46@gmail.com>
2020-12-06 23:18:08 +0100

    delete unit tests

--- test/test_convert.py
+++ test/test_convert.py
@@ -17,15 +17,3 @@ def test1():
 
     subtree = ts.get_subtree(nested2, 3)
     assert subtree == [[3, 4, 7, 1, 'C'], [4, 5, 6, 2, 'D']]
-
-
-def test2():
-    nested = [
-        [1, 1, 8, 0, 'a'], [2, 2, 5, 1, 'b'],
-        [4, 3, 4, 2, 'd'], [3, 6, 7, 1, 'c']]
-    subtrees = ts.extract_all_subtrees(nested)
-    assert subtrees == [
-        [[1, 8, 0, 'a'], [2, 5, 1, 'b'], [3, 4, 2, 'd'], [6, 7, 1, 'c']],
-        [[1, 4, 0, 'b'], [2, 3, 1, 'd']],
-        [[1, 2, 0, 'd']],
-        [[1, 2, 0, 'c']]]
