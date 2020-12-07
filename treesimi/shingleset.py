65d6ac98d8e4f964773d85e6072e89cc1bef7c61
UH <554c46@gmail.com>
2020-12-07 20:55:08 +0100

    make augmentations optional

--- treesimi/shingleset.py
+++ treesimi/shingleset.py
@@ -7,6 +7,9 @@ DATA = Union[int, float, str, dict, list, tuple]
 
 
 def shingleset(nested: List[Tuple[int, int, int, DATA]],
+               use_trunc_leaves: Optional[bool] = True,
+               use_drop_nodes: Optional[bool] = True,
+               use_replace_attr: Optional[bool] = True,
                placeholder: Optional[str] = '\uFFFF',
                ) -> List[List[Tuple[int, int, int, DATA]]]:
     if len(nested[0]) == 5:
@@ -18,19 +21,22 @@ def shingleset(nested: List[Tuple[int, int, int, DATA]],
     trees = unique_trees(trees)
 
     # Truncate leaves
-    for tmp in copy.deepcopy(trees):
-        trees.extend(trunc_leaves(tmp))
-    trees = unique_trees(trees)
+    if use_trunc_leaves:
+        for tmp in copy.deepcopy(trees):
+            trees.extend(trunc_leaves(tmp))
+        trees = unique_trees(trees)
 
     # Drop nodes
-    for tmp in copy.deepcopy(trees):
-        trees.extend(drop_nodes(tmp))
-    trees = unique_trees(trees)
+    if use_drop_nodes:
+        for tmp in copy.deepcopy(trees):
+            trees.extend(drop_nodes(tmp))
+        trees = unique_trees(trees)
 
     # Mask data attributes
-    for tmp in copy.deepcopy(trees):
-        trees.extend(replace_attr(tmp, placeholder=placeholder))
-    trees = unique_trees(trees)
+    if use_replace_attr:
+        for tmp in copy.deepcopy(trees):
+            trees.extend(replace_attr(tmp, placeholder=placeholder))
+        trees = unique_trees(trees)
 
     # done
     return trees
