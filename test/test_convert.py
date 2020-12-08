14c99a9408d5daf91b4166b2af86f506c088e162
UH <554c46@gmail.com>
2020-12-08 14:52:18 +0100

    fix #30

--- test/test_convert.py
+++ test/test_convert.py
@@ -1,4 +1,5 @@
 import treesimi as ts
+from treesimi.convert import adjac_to_nested_recur
 
 
 def test1():
@@ -17,3 +18,15 @@ def test1():
 
     subtree = ts.get_subtree(nested2, 3)
     assert subtree == [[3, 4, 7, 1, 'C'], [4, 5, 6, 2, 'D']]
+
+
+def test2():
+    adjac = [(1, 0), (2, 1), (3, 1), (4, 2)]
+    _, nested = adjac_to_nested_recur(
+        adjac, parent_id=1, lft=1, depth=0, nested=[])
+    assert [4, 3, 4, 2] in nested
+    assert [2, 2, 5, 1] in nested
+    assert [3, 6, 7, 1] in nested
+    assert [1, 1, 8, 0] in nested
+    assert len(nested) == 4
+
