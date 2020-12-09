950abdc2e2802735314e2a3c8b43ae0e1c30dfc9
UH <554c46@gmail.com>
2020-12-09 21:57:13 +0100

    fix #7

--- setup.py
+++ setup.py
@@ -2,14 +2,9 @@ from setuptools import setup
 import m2r
 
 
-def read(fname):
-    import os
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
 setup(name='treesimi',
       version='0.1.0',
-      description='Compute similarity between trees, e.g. dependency trees',
+      description='Compute similarity between netsted set based trees.',
       long_description=m2r.parse_from_file('README.md'),
       long_description_content_type='text/x-rst',
       url='http://github.com/ulf1/treesimi',
@@ -20,4 +15,4 @@ setup(name='treesimi',
       install_requires=[
           'setuptools>=40.0.0'],
       python_requires='>=3.6',
-      zip_safe=False)
+      zip_safe=True)
